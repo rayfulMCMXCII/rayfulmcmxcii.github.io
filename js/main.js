f36f5341cb0074954e34d124f47452f5cecf985f
rayfulMCMXCII <85059386+rayfulMCMXCII@users.noreply.github.com>
2021-06-12 16:48:07 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-//
+/* Open */
+function openNav() {
+  document.getElementById("location").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+  document.getElementById("location").style.height = "0%";
+}
