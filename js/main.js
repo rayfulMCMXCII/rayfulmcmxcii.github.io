6832d37f7ed70b637316454291445172c3e28d14
rayfulMCMXCII <85059386+rayfulMCMXCII@users.noreply.github.com>
2021-06-12 17:22:35 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-//
+function on() {
+  document.getElementById("locationOverlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("locationOverlay").style.display = "none";
+}
