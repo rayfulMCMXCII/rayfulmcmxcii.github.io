d323c6ed1f3e751db63f6089cf423581717b08a1
rayfulMCMXCII <85059386+rayfulMCMXCII@users.noreply.github.com>
2021-06-14 10:46:06 +0300

    Delete main.js

--- js/main.js
+++
@@ -1,7 +0,0 @@
-function on() {
-  document.getElementById("locationOverlay").style.display = "block";
-}
-
-function off() {
-  document.getElementById("locationOverlay").style.display = "none";
-}
